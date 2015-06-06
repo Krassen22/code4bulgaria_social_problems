3c3f53a4bfb30ab5fa97d8463f52d21884e1d40c
BRANIAC <dimmat97@gmail.com>
2015-06-06 20:53:47 +0300

    Full registration added

--- SocialProblems/app/src/main/java/shaolinspiders/socialproblems/Camera.java
+++ SocialProblems/app/src/main/java/shaolinspiders/socialproblems/Camera.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,8 +25,35 @@ public class Camera extends ActionBarActivity {
         setContentView(R.layout.camera);
         setIds();
         addListeners();
-        takePicture();
+        //takePicture();
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void logout() {
+        getSharedPreferences("token", MODE_PRIVATE).edit().clear().apply();
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
+    }
+
 
     private void addListeners() {
         next.setOnClickListener(nextListener);
--- SocialProblems/app/src/main/java/shaolinspiders/socialproblems/Register.java
+++ SocialProblems/app/src/main/java/shaolinspiders/socialproblems/Register.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,16 +21,26 @@ import http.HttpPostRequest;
 /**
  * Created by dimmat97 on 5/8/15.
  */
-public class Register extends Activity {
+public class Register extends ActionBarActivity {
 
     Button submit_button;
     EditText username_et;
     EditText password_et;
     EditText password_conf_et;
+    EditText firstName_et;
+    EditText secondName_et;
+    EditText lastName_et;
+    EditText town_et;
+    EditText address_et;
 
     private String username;
     private String password;
     private String conf_password;
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String town;
+    private String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +55,22 @@ public class Register extends Activity {
         username = username_et.getText().toString();
         password = password_et.getText().toString();
         conf_password = password_conf_et.getText().toString();
+        firstName = firstName_et.getText().toString();
+        secondName = secondName_et.getText().toString();
+        lastName = lastName_et.getText().toString();
+        town = town_et.getText().toString();
+        address = address_et.getText().toString();
     }
     private void setViews() {
         submit_button = (Button) findViewById(R.id.account_button);
         username_et = (EditText) findViewById(R.id.register_username);
         password_et = (EditText) findViewById(R.id.register_password);
         password_conf_et = (EditText) findViewById(R.id.register_password_confirm);
+        firstName_et = (EditText) findViewById(R.id.register_firstName);
+        secondName_et = (EditText) findViewById(R.id.register_secondName);
+        lastName_et = (EditText) findViewById(R.id.register_lastName);
+        town_et = (EditText) findViewById(R.id.register_town);
+        address_et = (EditText) findViewById(R.id.register_address);
     }
 
     View.OnClickListener register_listener = new View.OnClickListener() {
@@ -78,6 +99,12 @@ public class Register extends Activity {
                 params.put("email", username);
                 params.put("password", password);
                 params.put("confirm_password", conf_password);
+                params.put("firstName",firstName);
+                params.put("secondName", secondName);
+                params.put("lastName", lastName);
+                params.put("town", town);
+                params.put("address", address);
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
